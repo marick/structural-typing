a8c806ed0d4247ba93ddb35f5da676c5e2d6321f
Brian Marick <marick@exampler.com>
2015-05-18 13:07:07 -0500

    exclusions

--- project.clj
+++ project.clj
@@ -7,11 +7,11 @@
             :distribution :repo}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [marick/suchwow "2.2.0"]
-                 [bouncer "0.3.2"]]
+                 [marick/suchwow "2.2.0" :exclusions [org.clojure/clojure]]
+                 [bouncer "0.3.2" :exclusions [org.clojure/clojure]]]
 
   :profiles {:dev {:dependencies [[midje "1.7.0-beta1" :exclusions [org.clojure/clojure]]
-                                  [org.blancas/morph "0.3.0"]]}
+                                  [org.blancas/morph "0.3.0" :exclusions [org.clojure/clojure]]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.5.1 {:dependencies [[org.clojure/clojure "1.5.1"]]}
