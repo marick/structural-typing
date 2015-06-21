4cfb11e8923cc7be17fa5e17186ee1ccaa9d46c1
Brian Marick <marick@exampler.com>
2015-06-20 19:58:19 -0500

    A better way to evaluate

--- test/structural_typing/f_evaluate.clj
+++ test/structural_typing/f_evaluate.clj
@@ -12,8 +12,6 @@
   (subject/friendly-name :key) => :key
   (subject/friendly-name #'even?) => "even?"
 
-  (subject/friendly-name (subject/show-as "odd!!!!!" even?)) => "odd!!!!!"
-
   (let [f ( ( (fn [a] (fn [b] (fn [c] (+ a b c)))) 1) 2)]
     (subject/friendly-name f) => "your custom predicate")
 
@@ -41,7 +39,7 @@
           ( (:error-explainer result) result) => ":x should be `greater-than-3`; it is `3`"))
 
       (fact "functions tagged with names"
-        (let [lifted (subject/lift (subject/show-as "three" (fn [n] (> n 3))))
+        (let [lifted (subject/lift (->> (fn [n] (> n 3)) (subject/show-as "three")))
               result (e/run-left (lifted {:leaf-value 3 :path [:x :y]}))]
           ( (:error-explainer result) result) => "[:x :y] should be `three`; it is `3`"))))
 
@@ -65,38 +63,51 @@
     
   (fact "constructing a custom predicate"
     (fact "you can override the predicate-string argument"
-      (let [lifted (subject/lift (comp even? pos?) {:predicate-string "evenish"})
+      
+      (let [my-variant-predicate (->> even? (subject/show-as "evenish"))
+            lifted (subject/lift my-variant-predicate)
             result (e/run-left (lifted {:leaf-value "string" :path [:x]}))]
-        result => (contains {:predicate-string "evenish"
+        result => (contains {:predicate (exactly even?)
+                             :predicate-string "evenish"
                              :path [:x]
                              :leaf-value "string"
                              :error-explainer subject/default-error-explainer})
         ( (:error-explainer result) result) => ":x should be `evenish`; it is `\"string\"`"))
 
     (fact "you can override the error-explainer argument"
-      (let [lifted (subject/lift even? {:error-explainer (fn [{:keys [predicate-string
-                                                                      path
-                                                                      leaf-value]}]
-                                                           (format "%s - %s - %s" path predicate-string leaf-value))})
+      (let [explainer (fn [{:keys [predicate-string
+                                   path
+                                   leaf-value]}]
+                        (format "%s - %s - %s" path predicate-string leaf-value))
+            lifted (subject/lift (subject/explain-with explainer even?))
             result (e/run-left (lifted {:leaf-value 3 :path [:x]}))]
 
+        result => (contains {:predicate (exactly even?)
+                             :predicate-string "core/even?"
+                             :path [:x]
+                             :leaf-value 3})
         ( (:error-explainer result) result) => "[:x] - core/even? - 3"))
 
     (fact "you can add arbitrary arguments" 
       (let [member (fn [& args]
-                     {:pred #(some (set args) %)
-                      :error-explainer (fn [{:keys [path leaf-value]}]
-                                         (format "%s (`%s`) should be a member of %s",
-                                                 (subject/friendly-path path)
-                                                 (pr-str leaf-value)
-                                                 (pr-str args)))})
-            instance (member 1 2 3)
-            lifted (subject/lift (:pred instance) (dissoc instance :pred))
+                     (->> #(some (set args) %)
+                          (subject/explain-with
+                           (fn [{:keys [path leaf-value]}]
+                             (format "%s (`%s`) should be a member of %s",
+                                     (subject/friendly-path path)
+                                     (pr-str leaf-value)
+                                     (pr-str args))))))
+            lifted (subject/lift (member 1 2 3))
             result (e/run-left (lifted {:leaf-value 8 :path [:x]}))]
-        ((:error-explainer result) result) => ":x (`8`) should be a member of (1 2 3)"))
-        
-))
-;; (fact "evaluating multiple predicates short circuits"
+        result => (contains {:predicate fn?
+                             :predicate-string "your custom predicate"
+                             :path [:x]
+                             :leaf-value 8
+                             :error-explainer fn?})
+
+        ((:error-explainer result) result) => ":x (`8`) should be a member of (1 2 3)"))))
+
+;; (future-fact "evaluating multiple predicates short circuits"
 ;;   (let [lifted (subject/life-predicates [pos? even?] [:x])
 ;;         result (e/run-left (lifted -1 {:x 1}))]
 ;;     result => {:predicate pos?
