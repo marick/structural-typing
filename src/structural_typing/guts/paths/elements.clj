151d07e14afd884af4ccb8a4efefe6b897f3b7a3
Brian Marick <marick@exampler.com>
2015-08-01 16:58:51 -0500

    Add RANGE

--- src/structural_typing/guts/paths/elements.clj
+++ src/structural_typing/guts/paths/elements.clj
@@ -4,6 +4,13 @@
             [such.metadata :as meta])
   (:require [com.rpl.specter :as specter]))
 
+(defn mkfn:meta-getter [key]
+  (fn [elt] (meta/get elt key)))
+
+(def offset (mkfn:meta-getter :offset))
+(def specter-equivalent (mkfn:meta-getter :specter-equivalent))
+(def will-match-many? (comp boolean (mkfn:meta-getter :will-match-many?)))
+
 (def ALL
   "Use this in a path to select all values of a 
    collection.
@@ -12,10 +19,12 @@
 "
   (meta/assoc 'ALL :specter-equivalent [specter/ALL] :offset 0 :will-match-many? true))
 
-(defn mkfn:meta-getter [key]
-  (fn [elt] (meta/get elt key)))
-
-(def offset (mkfn:meta-getter :offset))
-(def specter-equivalent (mkfn:meta-getter :specter-equivalent))
-(def will-match-many? (comp boolean (mkfn:meta-getter :will-match-many?)))
-
+;; TODO: In suchwow 3, have to use `instead-of`, which is lame, because it has global
+;; state. such.readable needs fixing.
+(defn RANGE [inclusive-start exclusive-end]
+  (let [r (meta/assoc 'RANGE
+                      :specter-equivalent [(specter/srange inclusive-start exclusive-end) specter/ALL]
+                      :offset inclusive-start
+                      :will-match-many? true)]
+    (readable/instead-of r (list 'RANGE inclusive-start exclusive-end))
+    r))
--- test/structural_typing/guts/paths/f_elements.clj
+++ test/structural_typing/guts/paths/f_elements.clj
@@ -1,9 +1,22 @@
 (ns structural-typing.guts.paths.f-elements
-  (:require [structural-typing.guts.paths.elements :as subject])
+  (:require [structural-typing.guts.paths.elements :as subject]
+            [structural-typing.guts.paths.readable :as readable])
   (:use midje.sweet))
 
 (facts "ALL"
   (fact "has an offset of 0"
     (subject/offset subject/ALL) => 0)
   (fact "matches many"
-    (subject/will-match-many? subject/ALL) => true))
+    (subject/will-match-many? subject/ALL) => true)
+  (fact "readable"
+    (readable/friendly [subject/ALL]) => "[ALL]"))
+
+(fact "RANGE"
+  (let [r (subject/RANGE 3 5)]
+    (fact "has a given offset"
+      (subject/offset r) => 3)
+    (fact "matches many"
+      (subject/will-match-many? r) => true)
+    (fact "readable"
+      (readable/friendly [r :x]) => "[(RANGE 3 5) :x]")))
+  
