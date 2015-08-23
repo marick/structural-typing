f08de4e3d2e962682b5382bfc7814aed08128087
Brian Marick <marick@exampler.com>
2015-08-23 14:02:09 -0500

    Document a future change

--- test/structural_typing/docs/f_readme.clj
+++ test/structural_typing/docs/f_readme.clj
@@ -2,7 +2,7 @@
  (:require [structural-typing.type :refer :all]
            [structural-typing.global-type :refer :all]
            [clojure.string :as str])
- (:use midje.sweet))
+ (:use midje.sweet structural-typing.pred-writing.testutil))
 
 
 (start-over!)
@@ -10,9 +10,9 @@
 (type! :Point {:x integer? :y integer?})
 
 (fact
-  (let [result (with-out-str (checked :Point {:x "one" :y "two"}))]
-    result => #":y should be `integer.*two"
-    result => #":x should be `integer.*one"))
+  (check-for-explanations :Point {:x "one" :y "two"})
+  => (just #":x should be `integer.*one"
+           #":y should be `integer.*two"))
 
 
 ;;; The throwing error handler.
@@ -40,9 +40,8 @@
 (type! :Point {:x [integer? pos?] :y [integer? pos?]})
 
 (fact
-  (let [result (with-out-str (checked :Point {:x -1 :y 2}))]
-    result => #":x should be `pos\?`"))
-
+  (check-for-explanations :Point {:x -1 :y 2})
+  => (just #":x should be `pos\?`"))
 
 (fact "map descriptions leave keys optional"
   (map #(checked :Point %) [{} {:x 1} {:y 1} {:x 1 :y 2}])
@@ -56,18 +55,18 @@
                :y [required-key integer?]})
 
 (fact
-  (let [result (with-out-str (checked :Point {:x "1"}))]
-    result => #":y must exist and be non-nil"
-    result => #":x should be `integer"))
+  (check-for-explanations :Point {:x "1"})
+  => (just #":x should be `integer"
+           #":y must exist and be non-nil"))
 
 (type! :Point
        [:x :y]
        {:x integer? :y integer?})
 
-(fact 
-  (let [result (with-out-str (checked :Point {:x "1"}))]
-    result => #":y must exist and be non-nil"
-    result => #":x should be `integer"))
+(fact
+  (check-for-explanations :Point {:x "1"})
+  => (just #":x should be `integer"
+           #":y must exist and be non-nil"))
 
 (fact "excess keys are fine"
   (map #(checked :Point %) [{:x 1 :y 2}
@@ -85,32 +84,46 @@
            :color [required-key string?]})
 
 (fact
-  (let [result (with-out-str (checked :ColorfulPoint {:y 1 :color 1}))]
-    result => #":x must exist"
-    result => #":color should be `string"))
-
+  (check-for-explanations :ColorfulPoint {:y 1 :color 1})
+  => (just #":color should be `string"
+           #":x must exist"))
 
 (type! :ColorfulPoint
               (includes :Point)
               {:color string?})
 (fact
-  (let [result (with-out-str (checked :ColorfulPoint {:y 1 :color 1}))]
-    result => #":x must exist"
-    result => #":color should be `string"))
+  (check-for-explanations :ColorfulPoint {:y 1 :color 1})
+  => (just #":color should be `string"
+           #":x must exist"))
 
 (type! :Colorful {:color [required-key string?]})
 (type! :ColorfulPoint (includes :Point) (includes :Colorful))
 
 (fact
-  (let [result (with-out-str (checked :ColorfulPoint {:y 1 :color 1}))]
-    result => #":x must exist"
-    result => #":color should be `string"))
+  (check-for-explanations :ColorfulPoint {:y 1 :color 1})
+  => (just #":color should be `string"
+           #":x must exist"))
 
 (fact "combining types at check time"
-  (let [result (with-out-str (checked [:Colorful :Point] {:y 1 :color 1}))]
-    result => #":x must exist"
-    result => #":color should be `string"))
+  (check-for-explanations [:Colorful :Point] {:y 1 :color 1})
+  => (just #":color should be `string"
+           #":x must exist"))
 
+;;; Sequences of maps
+
+(type! :Points {[ALL :x] integer?
+                [ALL :y] integer?})
+(type! :Points2 {[ALL] (includes :Point)})
+
+(fact "incorrect paths"
+  (check-for-explanations :Points 3) => (just #"\[ALL :x] is not a path into `3`"
+                                              #"\[ALL :y] is not a path into `3`")
+
+  (check-for-explanations :Points2 3) => (just #"\[ALL :x] is not a path into `3`"
+                                               #"\[ALL :y] is not a path into `3`")
+
+  (check-for-explanations :Points [1 2 3]) =future=> (just #"\[ALL :x] is not a path into `3`"
+                                                           #"\[ALL :y] is not a path into `3`"))
 
 ;;; Nesting types and key paths
 
@@ -127,26 +140,24 @@
 (fact 
   (checked :Figure ok-figure) => ok-figure
 
-  (let [result (with-out-str (checked :Figure {:points [{:y 1} {:x 1 :y "2"}]}))]
-    result => #":color must exist"
-    result => #"\[:points 0 :x\] must exist"
-    result => #"\[:points 1 :y\] should be `integer\?`"))
+  (check-for-explanations :Figure {:points [{:y 1} {:x 1 :y "2"}]})
+  => (just #":color must exist"
+           #"\[:points 0 :x\] must exist"
+           #"\[:points 1 :y\] should be `integer\?`"))
 
 (fact "a sorted result"
-  (let [result (with-out-str (checked :Figure {:points {:x 1 :y 2}}))]
-    result =>
-":color must exist and be non-nil
-[:points 0 :x] must exist and be non-nil
-[:points 0 :y] must exist and be non-nil
-[:points 1 :x] must exist and be non-nil
-[:points 1 :y] must exist and be non-nil
-"))
+  (check-for-explanations :Figure {:points {:x 1 :y 2}})
+  => (just ":color must exist and be non-nil"
+           "[:points 0 :x] must exist and be non-nil"
+           "[:points 0 :y] must exist and be non-nil"
+           "[:points 1 :x] must exist and be non-nil"
+           "[:points 1 :y] must exist and be non-nil"))
     
 (fact "bad paths"
-  (let [result (with-out-str (checked :Figure {:points 3}))]
-    result => #":color must exist"
-    result => #"\[:points ALL :x\] is not a path into `\{:points 3\}`"
-    result => #"\[:points ALL :y\] is not a path into `\{:points 3\}`"))
+  (check-for-explanations :Figure {:points 3})
+   => (just #":color must exist"
+            #"\[:points ALL :x\] is not a path into `\{:points 3\}`"
+            #"\[:points ALL :y\] is not a path into `\{:points 3\}`"))
     
 
 (start-over!)
