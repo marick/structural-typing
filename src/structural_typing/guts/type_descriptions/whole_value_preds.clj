cbf03fac5a996a4753e59f1e2ec19a2c96bc63b9
Brian Marick <marick@exampler.com>
2015-09-15 16:42:38 -0500

    More dead code

--- src/structural_typing/guts/type_descriptions/whole_value_preds.clj
+++
@@ -1,6 +0,0 @@
-(ns structural-typing.guts.type-descriptions.whole-value-preds
-  (:use structural-typing.clojure.core))
-
-(def dc:preds->maps
-  (lazyseq:x->y #(hash-map [] [%]) extended-fn?))
-
--- test/structural_typing/guts/type_descriptions/f_whole_value_preds.clj
+++
@@ -1,9 +0,0 @@
-(ns structural-typing.guts.type-descriptions.f-whole-value-preds
-  (:require [structural-typing.guts.type-descriptions.whole-value-preds :as subject])
-  (:use midje.sweet))
-
-(fact dc:preds->maps
-  (subject/dc:preds->maps [even?]) => (just {[] [even?]})
-  (subject/dc:preds->maps [[:a [:b]] {:a odd?} even?])
-  =>                      [[:a [:b]] {:a odd?} {[] [even?]}]) 
-
