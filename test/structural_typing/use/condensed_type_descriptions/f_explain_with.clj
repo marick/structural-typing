338fab200b261613fdbf8ae49d3d6f96044ce15b
Brian Marick <marick@exampler.com>
2015-09-18 15:10:28 -0500

    Improve tests

---
+++ test/structural_typing/use/condensed_type_descriptions/f_explain_with.clj
@@ -0,0 +1,22 @@
+(ns structural-typing.use.condensed-type-descriptions.f-explain-with
+  (:require [structural-typing.assist.oopsie :as oopsie])
+  (:use midje.sweet
+        structural-typing.type
+        structural-typing.global-type
+        structural-typing.clojure.core
+        structural-typing.assist.testutil))
+
+(start-over!)
+
+(def good-password? (->> (partial re-find #"\d")
+                         (explain-with 
+                          (fn [oopsie] 
+                            (format "%s should contain a digit" 
+                                    (oopsie/friendly-path oopsie))))))
+
+(type! :Account {:password good-password?})
+(fact
+  (with-out-str (checked :Account {:password "foo"}))
+  => #":password should contain a digit")
+
+(start-over!)
---
+++ test/structural_typing/use/condensed_type_descriptions/f_show_as.clj
@@ -0,0 +1,25 @@
+(ns structural-typing.use.condensed-type-descriptions.f-show-as
+  (:use midje.sweet
+        structural-typing.type
+        structural-typing.global-type
+        structural-typing.clojure.core
+        structural-typing.assist.testutil))
+
+(start-over!)
+
+
+(fact "without `show-as`"
+  (def good-password? (partial re-find #"\d"))
+  (type! :Account {:password good-password?})
+  
+  (check-for-explanations :Account {:password "fool"})
+  => [(err:shouldbe :password "<custom-predicate>" "\"fool\"")])
+
+(fact "a typical use of show-as"
+  (type! :Address 
+         {:name (show-as "short string" #(<= % 10))})
+
+  (check-for-explanations :Address {:name "llllllllllllllllll"})
+  => [(err:shouldbe :name "short string" "\"llllllllllllllllll\"")])
+
+(start-over!)
