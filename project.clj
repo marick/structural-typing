1b819a66f0b19d67667122d573f6806f17000ee7
Brian Marick <marick@exampler.com>
2015-09-29 16:42:27 -0500

    Bump version to 0.13.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marick/structural-typing "0.12.0"
+(defproject marick/structural-typing "0.13.0"
   :description "Define types by giving descriptions (perhaps incomplete) of how they're built.
                \"...by how they're built\" means applying arbitrary predicates (especially ones
                about existence) to parts of structures.
@@ -23,7 +23,7 @@
 
   :profiles {:dev {:dependencies [[midje "1.8-alpha1" :exclusions [org.clojure/clojure]]
                                   [org.blancas/morph "0.3.0" :exclusions [org.clojure/clojure]]
-                                  [com.taoensso/timbre "4.1.1" :exclusions [org.clojure/clojure]]
+                                  [com.taoensso/timbre "4.1.2" :exclusions [org.clojure/clojure]]
                                   [org.clojure/math.numeric-tower "0.0.4"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
