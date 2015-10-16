92dae1516ffc8b815dd28aacc4dd32e716b242b7
Brian Marick <marick@exampler.com>
2015-10-16 16:48:32 -0500

    Made a test for how it’s possible to test for nil

---
+++ test/structural_typing/use/condensed_type_descriptions/f_nil_checks.clj
@@ -0,0 +1,17 @@
+(ns structural-typing.use.condensed-type-descriptions.f-nil-checks
+  (:use midje.sweet
+        structural-typing.type
+        structural-typing.global-type
+        structural-typing.clojure.core
+        structural-typing.assist.testutil))
+
+(start-over!)
+
+(fact "It is possible to check for nil values (even though those are normally optional)"
+  (type! :X {:x nil?})
+  (built-like :X {:x nil}) => {:x nil}
+  (built-like :X {}) => {}
+  (check-for-explanations :X {:x 1}) => (just (err:shouldbe :x "nil?" 1)))
+
+
+(start-over!)
