9de2b8126d62c616bec23347f7452afb3e6ca355
Brian Marick <marick@exampler.com>
2016-03-15 18:34:26 -0500

    Rearrange timbre examples

--- examples/timbre_define_3.clj
+++ examples/timbre_define_3.clj
@@ -1,5 +1,5 @@
-(ns timbre-define-2
-  "Logging to Timbre - multi-line messages that contains the whole value"
+(ns timbre-define-3
+  "Logging to Timbre - as with 2, but each line in a different log message"
   ;; Because this is all about tailoring structural-typing, the rare `:refer :all` is appropriate:
   (:use structural-typing.type)
 
@@ -11,23 +11,19 @@
 
 (timbre/set-level! :info)
 
-;; Example 2: print a header line, followed by N explanations
-
-;; The explainer is given N oopsies, but they all come from
-;; the same original value, called `:whole-value`.
-;; Note N is always at least 1.
-(defn whole-value [oopsies]
-  (:whole-value (first oopsies)))
+;; Example 3: print multiple individual messages, followed by a summary
 
 (def pprint-to-string #(with-out-str (pprint %)))
 
 (defn error-explainer [oopsies]
-  (let [intro-line "Type failure while checking this:"
-        whole-value-line (pprint-to-string (whole-value oopsies))]
-    (->> (oopsie/explanations oopsies)
-         (into [intro-line whole-value-line])
-         (str/join "\n")
-         timbre/error)))
+  (timbre/info "While checking this:")
+  (-> (first oopsies) ; the error handler is always given at least one oopsie.
+      :whole-value    ; the original candidate being checked
+      pprint-to-string
+      str/trimr       ; be tidy by getting rid of pprint's trailing newline
+      timbre/info)
+  (doseq [e (oopsie/explanations oopsies)] (timbre/info e))
+  (timbre/error "Type check failed - see preceding messages."))
 
 (def type-repo
   (-> empty-type-repo
--- examples/timbre_use.clj
+++ examples/timbre_use.clj
@@ -1,7 +1,8 @@
 (ns timbre-use
   "An example of logging to Timbre"
   (:require [timbre-define-1 :as v1]
-            [timbre-define-2 :as v2])
+            [timbre-define-2 :as v2]
+            [timbre-define-3 :as v3])
   (:use midje.sweet))
 
 (fact "log each explanation as an error"
@@ -10,11 +11,20 @@
     result => #"ERROR \[timbre-define-1:\d+\] - :x should be `integer\?`; it is `\"1\"`"
     result => #"ERROR \[timbre-define-1:\d+\] - :y does not exist"))
 
-(fact "more verbose error handling"
+(fact "a single message; summary first, followed by lines of explanation"
   (let [result (with-out-str (v2/built-like :Point {:x "1"}))]
+    ; (println result)
+    result => #"ERROR.* - Type failure while checking this:"
+    result => #"\{:x \"1\"\}"
+    result => #":x should be `integer"
+    result => #":y does not exist"))
+
+
+(fact "each error line + a summary message"
+  (let [result (with-out-str (v3/built-like :Point {:x "1"}))]
     ; (println result)
     result => #"INFO.* - While checking this:"
     result => #"INFO.* - \{:x \"1\"\}"
-    result => #"INFO \[timbre-define-2:\d+\] - :x should be `integer"
-    result => #"INFO \[timbre-define-2:\d+\] - :y does not exist"
-    result => #"ERROR \[timbre-define-2:\d+\] - Boundary type check failed"))
+    result => #"INFO \[timbre-define-3:\d+\] - :x should be `integer"
+    result => #"INFO \[timbre-define-3:\d+\] - :y does not exist"
+    result => #"ERROR \[timbre-define-3:\d+\] - Type check failed"))
